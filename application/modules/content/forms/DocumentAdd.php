f76fd60acb3c5e841f531566847a8224f46908ce
got <got@a2e456e6-18a6-49bb-b7a0-2c45267b191c>
2011-11-21 21:48:38 +0000

    start manage documents
    git-svn-id: svn://88.190.234.191/escms@17 a2e456e6-18a6-49bb-b7a0-2c45267b191c

---
+++ application/modules/content/forms/DocumentAdd.php
@@ -0,0 +1,32 @@
+<?php
+class Content_Form_DocumentAdd extends Es_Form
+{
+    public function init()
+    {
+        $this->setMethod(self::METHOD_POST);
+        $this->setElementsBelongTo('document');
+
+        $name = new Zend_Form_Element_Text('name');
+        $name->setRequired(TRUE)
+            ->setLabel('Name')
+            ->setAttrib('class', 'input-text')
+            ->addValidator(new Zend_Validate_NotEmpty());
+
+        $url_key  = new Zend_Form_Element_Text('url_key');
+        $url_key->setRequired(TRUE)
+            ->setLabel('Url key')
+            ->setAttrib('class', 'input-text')
+            ->addValidator(new Zend_Validate_NotEmpty());
+
+        $document_collection = new Es_Model_DbTable_Document_Collection();
+        $parent_id = new Zend_Form_Element_Select('parent_id');
+        $parent_id->addMultiOption('', 'Select parent');
+        $parent_id->addMultiOptions($document_collection->getSelect());
+
+        $submit = new Zend_Form_Element_Submit('submit');
+        $submit->setAttrib('class', 'input-submit')
+            ->setLabel('Create');
+
+        $this->addElements(array($name, $url_key, $parent_id, $submit));
+    }
+}
