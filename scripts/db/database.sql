a8542e73cb0e45dd7ef28997d3eef89580f3a81c
got <got@a2e456e6-18a6-49bb-b7a0-2c45267b191c>
2011-11-28 07:11:59 +0000

    Taks #4: Database schema
    git-svn-id: svn://88.190.234.191/escms@22 a2e456e6-18a6-49bb-b7a0-2c45267b191c

--- scripts/db/database.sql
+++ scripts/db/database.sql
@@ -6,49 +6,6 @@
 -- Description: 
 ------------------------------
 
-
--- Start Séquence's declaration
-DROP SEQUENCE IF EXISTS "datatypes_id_seq" CASCADE;
-CREATE SEQUENCE "datatypes_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
-
-DROP SEQUENCE IF EXISTS "document_types_document_type_id_seq" CASCADE;
-CREATE SEQUENCE "document_types_document_type_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
-
-DROP SEQUENCE IF EXISTS "documents_id_seq" CASCADE;
-CREATE SEQUENCE "documents_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
-
-DROP SEQUENCE IF EXISTS "icons_id_seq" CASCADE;
-CREATE SEQUENCE "icons_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
-
-DROP SEQUENCE IF EXISTS "layouts_id_seq" CASCADE;
-CREATE SEQUENCE "layouts_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
-
-DROP SEQUENCE IF EXISTS "models_id_seq" CASCADE;
-CREATE SEQUENCE "models_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
-
-DROP SEQUENCE IF EXISTS "properties_id_seq" CASCADE;
-CREATE SEQUENCE "properties_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
-
-DROP SEQUENCE IF EXISTS "properties_value_id_seq" CASCADE;
-CREATE SEQUENCE "properties_value_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
-
-DROP SEQUENCE IF EXISTS "tab_id_seq" CASCADE;
-CREATE SEQUENCE "tab_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
-
-DROP SEQUENCE IF EXISTS "users_id_seq" CASCADE;
-CREATE SEQUENCE "users_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
-
-DROP SEQUENCE IF EXISTS "views_id_seq" CASCADE;
-CREATE SEQUENCE "views_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 CYCLE;
-
-DROP SEQUENCE IF EXISTS "translate_id_seq" CASCADE;
-CREATE SEQUENCE "translate_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE CYCLE;
-
-DROP SEQUENCE IF EXISTS "translate_language_id_seq" CASCADE;
-CREATE SEQUENCE "translate_language_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE CYCLE;
-
--- End Séquence's declaration
-
 -- Start Table's declaration
 DROP TABLE IF EXISTS "datatypes" CASCADE;
 CREATE TABLE "datatypes" (
@@ -215,53 +172,81 @@ CREATE UNIQUE INDEX "translate_language_idx" ON "translate_language" USING btree
 -- End Table's declaration
 
 -- Start Relation's declaration
-ALTER TABLE "datatypes" DROP CONSTRAINT "datatypes_model_id" CASCADE;
 ALTER TABLE "datatypes" ADD CONSTRAINT "datatypes_model_id" FOREIGN KEY ("model_id") REFERENCES "models"("id") ON UPDATE RESTRICT ON DELETE RESTRICT;
 
-ALTER TABLE "document_types" DROP CONSTRAINT "document_types_icon_id" CASCADE;
 ALTER TABLE "document_types" ADD CONSTRAINT "document_types_icon_id" FOREIGN KEY ("icon_id") REFERENCES "icons"("id") ON UPDATE RESTRICT ON DELETE RESTRICT;
 
-ALTER TABLE "document_types" DROP CONSTRAINT "document_types_view_id" CASCADE;
 ALTER TABLE "document_types" ADD CONSTRAINT "document_types_view_id" FOREIGN KEY ("default_view_id") REFERENCES "views"("id") ON UPDATE RESTRICT ON DELETE RESTRICT;
 
-ALTER TABLE "document_type_views" DROP CONSTRAINT "document_type_views_document_types_id" CASCADE;
 ALTER TABLE "document_type_views" ADD CONSTRAINT "document_type_views_document_types_id" FOREIGN KEY ("id") REFERENCES "document_types"("id") ON UPDATE RESTRICT ON DELETE RESTRICT;
 
-ALTER TABLE "document_type_views" DROP CONSTRAINT "document_type_views_view_id" CASCADE;
 ALTER TABLE "document_type_views" ADD CONSTRAINT "document_type_views_view_id" FOREIGN KEY ("view_id") REFERENCES "views"("id") ON UPDATE RESTRICT ON DELETE RESTRICT;
 
-ALTER TABLE "documents" DROP CONSTRAINT "documents_layout_id" CASCADE;
 ALTER TABLE "documents" ADD CONSTRAINT "documents_layout_id" FOREIGN KEY ("layout_id") REFERENCES "layouts"("id") ON UPDATE RESTRICT ON DELETE RESTRICT;
 
-ALTER TABLE "documents" DROP CONSTRAINT "document_types_id" CASCADE;
 ALTER TABLE "documents" ADD CONSTRAINT "document_types_id" FOREIGN KEY ("document_type_id") REFERENCES "document_types"("id") ON UPDATE RESTRICT ON DELETE RESTRICT;
 
-ALTER TABLE "documents" DROP CONSTRAINT "documents_view_id" CASCADE;
 ALTER TABLE "documents" ADD CONSTRAINT "documents_view_id" FOREIGN KEY ("view_id") REFERENCES "views"("id") ON UPDATE RESTRICT ON DELETE RESTRICT;
 
-ALTER TABLE "properties" DROP CONSTRAINT "properties_datatype_id" CASCADE;
 ALTER TABLE "properties" ADD CONSTRAINT "properties_datatype_id" FOREIGN KEY ("datatype_id") REFERENCES "datatypes"("id") ON UPDATE RESTRICT ON DELETE RESTRICT;
 
-ALTER TABLE "properties" DROP CONSTRAINT "properties_tab_id" CASCADE;
 ALTER TABLE "properties" ADD CONSTRAINT "properties_tab_id" FOREIGN KEY ("tab_id") REFERENCES "tabs"("id") ON UPDATE RESTRICT ON DELETE RESTRICT;
 
-ALTER TABLE "properties_values" DROP CONSTRAINT "properties_values_document_id" CASCADE;
 ALTER TABLE "properties_values" ADD CONSTRAINT "properties_values_document_id" FOREIGN KEY ("document_id") REFERENCES "documents"("id") ON UPDATE RESTRICT ON DELETE RESTRICT;
 
-ALTER TABLE "properties_values" DROP CONSTRAINT "properties_values_property_id" CASCADE;
 ALTER TABLE "properties_values" ADD CONSTRAINT "properties_values_property_id" FOREIGN KEY ("property_id") REFERENCES "properties"("id") ON UPDATE RESTRICT ON DELETE RESTRICT;
 
-ALTER TABLE "tabs" DROP CONSTRAINT "tabs_document_types_id" CASCADE;
 ALTER TABLE "tabs" ADD CONSTRAINT "tabs_document_types_id" FOREIGN KEY ("document_type_id") REFERENCES "document_types"("id") ON UPDATE RESTRICT ON DELETE RESTRICT;
 
-ALTER TABLE "documents" DROP CONSTRAINT "documents_user_id" CASCADE;
 ALTER TABLE "documents" ADD CONSTRAINT "documents_user_id" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON UPDATE RESTRICT ON DELETE RESTRICT;
 
-ALTER TABLE "document_types" DROP CONSTRAINT "document_types_user_id" CASCADE;
 ALTER TABLE "document_types" ADD CONSTRAINT "document_types_user_id" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON UPDATE RESTRICT ON DELETE RESTRICT;
 
-ALTER TABLE "translate_language" DROP CONSTRAINT "translate_locale_id" CASCADE;
 ALTER TABLE "translate_language" ADD CONSTRAINT "translate_locale_id" FOREIGN KEY ("translate_id") REFERENCES "translate"("id") ON UPDATE CASCADE ON DELETE CASCADE;
 
 -- End Relation's declaration
+
+
+-- Start Séquence's declaration
+DROP SEQUENCE IF EXISTS "datatypes_id_seq" CASCADE;
+CREATE SEQUENCE "datatypes_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
+
+DROP SEQUENCE IF EXISTS "document_types_id_seq" CASCADE;
+CREATE SEQUENCE "document_types_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
+
+DROP SEQUENCE IF EXISTS "documents_id_seq" CASCADE;
+CREATE SEQUENCE "documents_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
+
+DROP SEQUENCE IF EXISTS "icons_id_seq" CASCADE;
+CREATE SEQUENCE "icons_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
+
+DROP SEQUENCE IF EXISTS "layouts_id_seq" CASCADE;
+CREATE SEQUENCE "layouts_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
+
+DROP SEQUENCE IF EXISTS "models_id_seq" CASCADE;
+CREATE SEQUENCE "models_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
+
+DROP SEQUENCE IF EXISTS "properties_id_seq" CASCADE;
+CREATE SEQUENCE "properties_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
+
+DROP SEQUENCE IF EXISTS "properties_value_id_seq" CASCADE;
+CREATE SEQUENCE "properties_value_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
+
+DROP SEQUENCE IF EXISTS "tabs_id_seq" CASCADE;
+CREATE SEQUENCE "tabs_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
+
+DROP SEQUENCE IF EXISTS "users_id_seq" CASCADE;
+CREATE SEQUENCE "users_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 NO CYCLE;
+
+DROP SEQUENCE IF EXISTS "views_id_seq" CASCADE;
+CREATE SEQUENCE "views_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE START 1 CYCLE;
+
+DROP SEQUENCE IF EXISTS "translate_id_seq" CASCADE;
+CREATE SEQUENCE "translate_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE CYCLE;
+
+DROP SEQUENCE IF EXISTS "translate_language_id_seq" CASCADE;
+CREATE SEQUENCE "translate_language_id_seq" INCREMENT 1 NO MINVALUE NO MAXVALUE CYCLE;
+
+-- End Séquence's declaration
+
 
