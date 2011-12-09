3996cbbd61fee7ea410a121eb10708ebc935b157
got <got@a2e456e6-18a6-49bb-b7a0-2c45267b191c>
2011-12-09 17:08:19 +0000

    Task #7: Créations de différents types de données - textarea
    git-svn-id: svn://88.190.234.191/escms@38 a2e456e6-18a6-49bb-b7a0-2c45267b191c

--- library/Datatypes/Textarea/Editor.php
+++ library/Datatypes/Textarea/Editor.php
@@ -3,7 +3,7 @@ class Datatypes_Textarea_Editor extends Es_Model_DbTable_Datatype_Abstract_Edito
 {
     public function save()
     {
-        $value = $request->getParam($this->getName());
+        $value = $this->getRequest()->getParam($this->getName());
         $this->setValue($value);
         return $this->saveValue();
     }
