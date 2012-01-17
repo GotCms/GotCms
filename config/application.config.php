5c24eabe7cecb1b67d65473ee54188a91ba55b70
got <got@a2e456e6-18a6-49bb-b7a0-2c45267b191c>
2012-01-17 22:54:45 +0000

    Task #15: Migrer le projet vers zf2 - Envoi des premiers fichiers
    git-svn-id: svn://88.190.234.191/escms@43 a2e456e6-18a6-49bb-b7a0-2c45267b191c

---
+++ config/application.config.php
@@ -0,0 +1,34 @@
+<?php
+return array(
+    'modules' => array(
+        'Application'
+        , 'Admin'
+        , 'Config'
+        , 'Content'
+        , 'Development'
+        , 'Statistics'
+    ),
+    'module_listener_options' => array(
+        'config_cache_enabled' => false
+        , 'cache_dir'            => 'data/cache'
+        , 'module_paths' => array(
+            './module'
+
+        ),
+    ),
+    'autoloader' => array(
+        'namespaces' => array(
+            'Es\\' => __DIR__ . '/../vendor/Es',
+        ),
+    ),
+    'database' => array(
+        'parameters' => array(
+            'config' => array(
+                'host' => 'localhost',
+                'username' => 'got',
+                'password' => 'x8maoxfp',
+                'dbname' => 'escms',
+            ),
+        ),
+    ),
+);
