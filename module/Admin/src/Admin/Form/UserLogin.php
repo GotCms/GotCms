fa89bfe303e57f00699304150d00684bc64e8b24
GoT <pierre.rambaud86@gmail.com>
2012-02-21 00:25:20 +0100

    User connection

---
+++ module/Admin/src/Admin/Form/UserLogin.php
@@ -0,0 +1,33 @@
+<?php
+namespace Admin\Form;
+
+use Es\Form\AbstractForm,
+    Zend\Validator\Db,
+    Zend\Form\Element;
+
+class UserLogin extends AbstractForm
+{
+    public function init()
+    {
+        $this->setMethod(self::METHOD_POST);
+
+        $email = new Element\Text('email');
+        $email->setRequired(TRUE)
+            ->setLabel('Email')
+            ->setAttrib('class', 'input-text')
+            ->addValidator('NotEmpty');
+
+        $password  = new Element\Password('password');
+        $password->setRequired(TRUE)
+            ->setLabel('Password')
+            ->setAttrib('class', 'input-text')
+            ->addValidator('NotEmpty');
+
+        $submit = new Element\Submit('submit');
+        $submit->setAttrib('class', 'input-submit')
+            ->setLabel('Log in');
+
+
+        $this->addElements(array($email, $password, $submit));
+    }
+}
