8d38af656de6a1fefc394403c2f8af41469c683e
GoT <pierre.rambaud86@gmail.com>
2012-02-27 22:56:32 +0100

    Missing Files

---
+++ module/Application/src/Application/Model/Datatype/AbstractDatatype/AbstractEditor.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Application\Model\Datatype\AbstractDatatype;
+
+use Es\Core\Object,
+    Application\Model\Datatype;
+
+abstract class AbstractEditor extends Object
+{
+    /**
+    * @var AbstractDatatype
+    */
+    protected $_datatype;
+
+    /**
+    * @var Es_Model_DbTable_Property_Model
+    */
+    protected $_property;
+
+    /**
+    * get name of datatype
+    * @var string
+    */
+    protected $_name;
+
+    abstract public function save();
+    abstract public function load();
+
+    public function __construct(Datatype\AbstractDatatype $datatype_abstract)
+    {
+        $this->_datatype = $datatype_abstract;
+        $this->_property = $datatype_abstract->getProperty();
+        parent::__construct();
+    }
+
+    protected function getValue()
+    {
+        return $this->getProperty()->getValue();
+    }
+
+    protected function setValue($value)
+    {
+        $this->getProperty()->setValue($value);
+
+        return $this;
+    }
+
+    protected function saveValue()
+    {
+        $value = $this->getValue();
+        if($this->getProperty()->isRequired() and empty($value))
+        {
+            return FALSE;
+        }
+
+        return $this->getProperty()->saveValue();
+    }
+
+    protected function getConfiguration()
+    {
+        return $this->getDatatype()->getParameters();
+    }
+
+    protected function setConfiguration($value)
+    {
+        $this->getDatatype()->setParameters($value);
+
+        return $this;
+    }
+
+    protected function getHelper($helper)
+    {
+        return $this->getDatatype()->getHelper($helper);
+    }
+
+    public function getUploadUrl()
+    {
+        return $this->getDatatype()->getUploadUrl().'/property/'.$this->getProperty()->getId();
+    }
+
+    public function getName()
+    {
+        return $this->getDatatype()->getName().$this->getProperty()->getId();
+    }
+
+    /**
+    * @return Es_Model_DbTable_Property_Model
+    */
+    public function getProperty()
+    {
+        return $this->_property;
+    }
+
+    /**
+    * @return Es_Model_DbTable_Property_Model
+    */
+    public function getDatatype()
+    {
+        return $this->_datatype;
+    }
+
+    /**
+    * @return Zend_Controller_Request_Http
+    */
+    public function getRequest()
+    {
+        return Zend_Controller_Front::getInstance()->getRequest();
+    }
+}
---
+++ module/Application/src/Application/Model/Datatype/AbstractDatatype/AbstractPrevalueEditor.php
@@ -0,0 +1,53 @@
+<?php
+namespace Application\Model\Datatype\AbstractDatatype;
+
+use Es\Core\Object,
+    Application\Model\Datatype;
+
+abstract class AbstractPrevalueEditor extends Object
+{
+    protected $_datatype;
+    protected $_config;
+
+    public function __construct(Datatype\AbstractDatatype $datatype_abstract)
+    {
+        $this->_datatype = $datatype_abstract;
+        parent::__construct();
+    }
+
+    abstract public function save();
+
+    abstract public function load();
+
+    protected function getConfig()
+    {
+        if(empty($this->_config))
+        {
+            $this->_config = unserialize($this->getDatatype()->getConfig());
+        }
+
+        return $this->_config;
+    }
+
+    protected function setConfig($value)
+    {
+        $this->getDatatype()->setConfig($value);
+        return $this;
+    }
+
+    /**
+    * @return Zend_Controller_Request_Http
+    */
+    public function getRequest()
+    {
+        return Zend_Controller_Front::getInstance()->getRequest();
+    }
+
+    /**
+    * @return Es_Model_DbTable_Datatype_Abstract
+    */
+    public function getDatatype()
+    {
+        return $this->_datatype;
+    }
+}
