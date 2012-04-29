d6252fd9cad5212aea5e792ffe8a28aab3835bb2
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2012-04-29 20:12:46 +0200

    IndexController Content

--- module/Content/src/Content/Controller/IndexController.php
+++ module/Content/src/Content/Controller/IndexController.php
@@ -2,10 +2,38 @@
 
 namespace Content\Controller;
 
-use Gc\Mvc\Controller\Action;
+use Gc\Mvc\Controller\Action,
+    Gc\Document\Collection as DocumentCollection,
+    Gc\Component,
+    Zend\Json\Json;
 
 class IndexController extends Action
 {
+    public function init()
+    {
+        $documents = new DocumentCollection();
+        $documents->load(0);
+
+        $this->layout()->setVariable('treeview',  Component\TreeView::render(array($documents)));
+
+        $routes = array(
+            'edit' => 'documentEdit'
+            , 'new' => 'documentAdd'
+            , 'delete' => 'documentDelete'
+            , 'copy' => 'documentCopy'
+            , 'cut' => 'documentCut'
+            , 'paste' => 'documentPaste'
+        );
+
+        $array_routes = array();
+        foreach($routes as $key => $route)
+        {
+            $array_routes[$key] = $this->url()->fromRoute($route, array('id' => 'itemId'));
+        }
+
+        $this->layout()->setVariable('routes', Json::encode($array_routes));
+    }
+
     public function indexAction()
     {
         return array('message' => 'azdazd');
