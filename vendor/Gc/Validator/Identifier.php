c60713f94f26eb4da1da705593f2fe13b87336d3
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2012-05-17 00:45:49 +0200

    \Gc\Validator\Identifier problem

--- vendor/Gc/Validator/Identifier.php
+++ vendor/Gc/Validator/Identifier.php
@@ -6,15 +6,10 @@ class Identifier extends AbstractValidator
 {
     const NOT_IDENTIFIER    = 'notIdentifier';
 
-    protected $_pattern = '~^[a-zA-Z0-9_-]+$~';
+    protected $_pattern = '~^[a-zA-Z0-9_]+$~';
     protected $_messageTemplates = array(
         self::NOT_IDENTIFIER => "'%value%' can only contains alphabetic characters and '_'"
     );
-
-    public function __construct()
-    {
-        parent::__construct();
-    }
 
     public function isValid($value)
     {
