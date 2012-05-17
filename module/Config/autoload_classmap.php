fea2aacdfa093e7642b08e7b49137ceed9a3144d
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2012-05-17 19:20:41 +0200

    License GPL integration

--- module/Config/autoload_classmap.php
+++ module/Config/autoload_classmap.php
@@ -1,6 +1,34 @@
 <?php
+/**
+ * This source file is part of Got CMS.
+ *
+ * Got CMS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Got CMS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with Got CMS. If not, see <http://www.gnu.org/licenses/gpl-3.0.html>.
+ *
+ * PHP Version >=5.3
+ *
+ * @category Gc
+ * @package  Config
+ * @author   Pierre Rambaud (GoT) <pierre.rambaud86@gmail.com>
+ * @license  GNU/GPL http://www.gnu.org/licenses/gpl-3.0.html
+ * @link     http://www.got-cms.com
+ * @license  http://www.gnu.org/licenses/gpl-3.0.html
+ */
+
 return array(
     'Config\Controller\IndexController' => __DIR__ . '/src/Config/Controller/IndexController.php',
-    'Config\Controller\ErrorController' => __DIR__ . '/src/Config/Controller/ErrorController.php',
+    'Config\Controller\CmsController'   => __DIR__ . '/src/Config/Controller/CmsController.php',
+    'Config\Controller\RoleController'  => __DIR__ . '/src/Config/Controller/RoleController.php',
+    'Config\Controller\UserController'  => __DIR__ . '/src/Config/Controller/UserController.php',
     'Config\Module'                     => __DIR__ . '/Module.php',
 );
