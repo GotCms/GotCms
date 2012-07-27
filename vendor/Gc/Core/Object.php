cd3ed913345422b9fbfc38c3260d6e24a079942f
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2012-07-27 19:33:11 +0200

    License

--- vendor/Gc/Core/Object.php
+++ vendor/Gc/Core/Object.php
@@ -1,19 +1,19 @@
 <?php
 /**
- * This source file is part of Got CMS.
+ * This source file is part of GotCms.
  *
- * Got CMS is free software: you can redistribute it and/or modify
+ * GotCms is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  *(at your option) any later version.
  *
- * Got CMS is distributed in the hope that it will be useful,
+ * GotCms is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License along
- * with Got CMS. If not, see <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * with GotCms. If not, see <http://www.gnu.org/licenses/lgpl-3.0.html>.
  *
  * PHP Version >=5.3
  *
