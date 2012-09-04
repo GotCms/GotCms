45d0deb32816e850a83045e638f842d82c7a9abc
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2012-09-04 20:00:42 +0200

    Missing file

---
+++ data/install/sql/data.sql
@@ -0,0 +1,30 @@
+------------------------------
+-- Data for GotCms
+--
+-- Icon values
+------------------------------
+
+INSERT INTO icon VALUES (1, 'Home', '/media/icons/home.png');
+INSERT INTO icon VALUES (2, 'Camera', '/media/icons/camera.png');
+INSERT INTO icon VALUES (3, 'Box', '/media/icons/box.png');
+INSERT INTO icon VALUES (4, 'Calendar', '/media/icons/calendar.png');
+INSERT INTO icon VALUES (5, 'Configuration', '/media/icons/configuration.png');
+INSERT INTO icon VALUES (6, 'File', '/media/icons/file.gif');
+INSERT INTO icon VALUES (7, 'Film', '/media/icons/film.png');
+INSERT INTO icon VALUES (8, 'Folder', '/media/icons/folder.gif');
+INSERT INTO icon VALUES (9, 'Folder closed', '/media/icons/folder-closed.gif');
+INSERT INTO icon VALUES (10, 'Image', '/media/icons/image.png');
+INSERT INTO icon VALUES (11, 'Letter blue', '/media/icons/letter-blue.png');
+INSERT INTO icon VALUES (12, 'Letter red', '/media/icons/letter-red.png');
+INSERT INTO icon VALUES (13, 'Pen green', '/media/icons/pen-green.png');
+INSERT INTO icon VALUES (14, 'Pen yellow', '/media/icons/pen-yellow.png');
+INSERT INTO icon VALUES (15, 'Printer', '/media/icons/printer.png');
+INSERT INTO icon VALUES (16, 'Rss', '/media/icons/rss.png');
+INSERT INTO icon VALUES (17, 'Save', '/media/icons/save-black.png');
+INSERT INTO icon VALUES (18, 'Save blue', '/media/icons/save-blue.png');
+INSERT INTO icon VALUES (19, 'Shell', '/media/icons/shell.png');
+INSERT INTO icon VALUES (20, 'Tool', '/media/icons/tool.png');
+INSERT INTO icon VALUES (21, 'Trash', '/media/icons/trash.png');
+INSERT INTO icon VALUES (22, 'Trash empty', '/media/icons/trash-empty.png');
+INSERT INTO icon VALUES (23, 'TV', '/media/icons/tv.png');
+INSERT INTO icon VALUES (24, 'Write', '/media/icons/write.png');
