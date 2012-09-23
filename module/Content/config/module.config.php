3982a8d883ef4a7c561866b894b1d5ef215aa71a
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2012-09-23 21:04:03 +0100

    #5 Documentation

--- module/Content/config/module.config.php
+++ module/Content/config/module.config.php
@@ -18,7 +18,7 @@
  * PHP Version >=5.3
  *
  * @category Gc
- * @package  Config
+ * @package  Content
  * @author   Pierre Rambaud (GoT) <pierre.rambaud86@gmail.com>
  * @license  GNU/LGPL http://www.gnu.org/licenses/lgpl-3.0.html
  * @link     http://www.got-cms.com
