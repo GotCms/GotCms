7658ccfb7d5ac48cf395467becd70722c979b43b
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2012-10-18 21:32:31 +0100

    #8 PHPUnit - database connection

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -1,5 +1,10 @@
 <?php
 
+use Gc\Core\Config as GcConfig,
+    Gc\Registry,
+    Zend\Db\Adapter\Adapter as DbAdapter,
+    Zend\Db\TableGateway\TableGateway;
+
 /*
  * Set error reporting to the level to which Es code must comply.
  */
@@ -43,6 +48,37 @@ else
 {
     require_once $gc_tests . DIRECTORY_SEPARATOR . 'TestConfiguration.php.dist';
 }
+
+$config = array();
+foreach(glob(realpath(__DIR__.'/../').'/config/autoload/{,*.}{global,local}.php', GLOB_BRACE) as $filename)
+{
+    $config += include_once($filename);
+}
+
+$config = array(
+    'db' => array(
+        'driver' => GC_DATABASE_DRIVER,
+        'username' => GC_DATABASE_USERNAME,
+        'password' => GC_DATABASE_PASSWORD,
+        'database' => GC_DATABASE_DATABASE,
+        'hostname' => GC_DATABASE_HOSTNAME,
+    ),
+);
+
+try
+{
+    $db_adapter = new DbAdapter($config['db']);
+}
+catch(Exception $e)
+{
+    echo 'Can\'t connect to database exiting.' . PHP_EOL;
+    exit;
+}
+
+\Zend\Db\TableGateway\Feature\GlobalAdapterFeature::setStaticAdapter($db_adapter);
+Registry::set('Configuration', $config);
+Registry::set('Db', $db_adapter);
+
 
 /**
  * Start output buffering, if enabled
