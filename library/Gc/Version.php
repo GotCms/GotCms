ec47e1ceb1dcead7c18e4f28d7cd2b272fde3e66
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2013-04-30 16:24:21 +0200

    Fixed #54 Missing version modification in Gc\Version

--- library/Gc/Version.php
+++ library/Gc/Version.php
@@ -40,7 +40,7 @@ final class Version
     /**
      * GotCms version identification - see compareVersion()
      */
-    const VERSION = '0.1.5';
+    const VERSION = '0.1.7';
 
     /**
      * The latest stable version GotCms available
