de1f579641331ae697d4417ba1a760dbb4720193
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2014-08-16 15:43:22 +0200

    #249 REST API
    Remove useless classes in namespaces

--- library/Gc/Mvc/Controller/Action.php
+++ library/Gc/Mvc/Controller/Action.php
@@ -27,14 +27,8 @@
 
 namespace Gc\Mvc\Controller;
 
-use Gc\Event\StaticEventManager;
-use Gc\Module\Model as ModuleModel;
-use Gc\User\Model as UserModel;
-use Gc\User\Role\Model as RoleModel;
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\Mvc\MvcEvent;
-use Zend\Session\Container as SessionContainer;
-use Zend\View\Model\JsonModel;
 
 /**
  * Extension of AbstractActionController
